5bbc6e90b9610ddbe4b56aa61bbe76f37544d0b4
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-09 14:57:27 +0300

    Add files via upload

---
+++ form24/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace form24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.rbCarp = new System.Windows.Forms.RadioButton();
+            this.rbCikarma = new System.Windows.Forms.RadioButton();
+            this.rbTopla = new System.Windows.Forms.RadioButton();
+            this.rbBol = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı 2:";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtSayi1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtSayi1.Location = new System.Drawing.Point(85, 21);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtSayi2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtSayi2.Location = new System.Drawing.Point(85, 64);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnHesapla.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHesapla.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnHesapla.Location = new System.Drawing.Point(39, 235);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(128, 41);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSonuc.Location = new System.Drawing.Point(36, 309);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(54, 17);
+            this.lblSonuc.TabIndex = 5;
+            this.lblSonuc.Text = "Sonuç:";
+            // 
+            // rbCarp
+            // 
+            this.rbCarp.AutoSize = true;
+            this.rbCarp.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCarp.Location = new System.Drawing.Point(15, 131);
+            this.rbCarp.Name = "rbCarp";
+            this.rbCarp.Size = new System.Drawing.Size(57, 21);
+            this.rbCarp.TabIndex = 6;
+            this.rbCarp.TabStop = true;
+            this.rbCarp.Text = "Çarp";
+            this.rbCarp.UseVisualStyleBackColor = true;
+            // 
+            // rbCikarma
+            // 
+            this.rbCikarma.AutoSize = true;
+            this.rbCikarma.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCikarma.Location = new System.Drawing.Point(108, 131);
+            this.rbCikarma.Name = "rbCikarma";
+            this.rbCikarma.Size = new System.Drawing.Size(79, 21);
+            this.rbCikarma.TabIndex = 7;
+            this.rbCikarma.TabStop = true;
+            this.rbCikarma.Text = "Çıkarma";
+            this.rbCikarma.UseVisualStyleBackColor = true;
+            // 
+            // rbTopla
+            // 
+            this.rbTopla.AutoSize = true;
+            this.rbTopla.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbTopla.Location = new System.Drawing.Point(15, 171);
+            this.rbTopla.Name = "rbTopla";
+            this.rbTopla.Size = new System.Drawing.Size(62, 21);
+            this.rbTopla.TabIndex = 8;
+            this.rbTopla.TabStop = true;
+            this.rbTopla.Text = "Topla";
+            this.rbTopla.UseVisualStyleBackColor = true;
+            // 
+            // rbBol
+            // 
+            this.rbBol.AutoSize = true;
+            this.rbBol.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBol.Location = new System.Drawing.Point(108, 171);
+            this.rbBol.Name = "rbBol";
+            this.rbBol.Size = new System.Drawing.Size(47, 21);
+            this.rbBol.TabIndex = 9;
+            this.rbBol.TabStop = true;
+            this.rbBol.Text = "Böl";
+            this.rbBol.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(257, 359);
+            this.Controls.Add(this.rbBol);
+            this.Controls.Add(this.rbTopla);
+            this.Controls.Add(this.rbCikarma);
+            this.Controls.Add(this.rbCarp);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.RadioButton rbCarp;
+        private System.Windows.Forms.RadioButton rbCikarma;
+        private System.Windows.Forms.RadioButton rbTopla;
+        private System.Windows.Forms.RadioButton rbBol;
+    }
+}
+
---
+++ form24/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float sayi1 = Convert.ToSingle(txtSayi1.Text);
+            float sayi2 = Convert.ToSingle(txtSayi2.Text);
+            float sonuc = 0;
+
+            if (rbCarp.Checked)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (rbCikarma.Checked)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (rbBol.Checked)
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            else if (rbTopla.Checked)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+             
+            lblSonuc.Text =sonuc.ToString();
+
+        }
+    }
+}
